78a58ac1ff5d5f10c0fb6d845a068ee5cf634090
Jihyun <hjh20030615@gmail.com>
2026-05-11 05:55:22 +0900

    feat: 마이페이지 UI 구현 완료

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -1,9 +1,270 @@
 import React from 'react';
+import Check from "../../assets/img/ic_green_check.png";
+import Shield from "../../assets/img/ic_orange_shield.png";
+import Network from "../../assets/img/ic_blue_network.png";
+import Wrench from "../../assets/img/ic_orange_wrench.png";
+import Setting from "../../assets/img/ic_purple_setting.png";
+import Goal from "../../assets/img/ic_orange_goal.png";
+import Alarm from "../../assets/img/ic_orange_alarm.png";
+import Key from "../../assets/img/ic_green_key.png";
+import Log from "../../assets/img/ic_gray_log.png";
 
 const MyPage = () => {
+    const userName = "김관리";
+    const userJob = "관리소장";
+    const location = "푸르지오 아파트 관리사무소";
+    const userId = "admin001";
+    const signupDate = "2024.01.15";
+    const authorityLevel = "Master";
+    const authorityLevelKr = "최고 관리자";
+    const onlineSensors = 5;
+    const offlineSensors = 1;
+    const averageBattery = 73;
+    const needAdjustment = 3;
+
     return (
         <div className='MyPage_Wrap'>
-            마이페이지입니다.
+            <div className="my_info">
+                <div className="title">마이페이지</div>
+                <div className="caption">개인 프로필 및 시스템 설정</div>
+                <div className="user_info">
+                    <div className="info_left">
+                        <div className="profile">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="user_name">{userName}</div>
+                        <div className="user_job">{userJob}</div>
+                        <div className="location">{location}</div>
+                        <div className="divider"></div>
+                        <div className="id_title">관리자 ID</div>
+                        <div className="user_id">{userId}</div>
+                        <div className="date_title">가입일</div>
+                        <div className="signup_date">{signupDate}</div>
+                    </div>
+                    <div className="info_right">
+                        <div className="authority_title">
+                            <div className="icon">
+                                <img src={Shield} alt="Shield" />
+                            </div>
+                            <div className="text">권한 관리</div>
+                        </div>
+                        <div className="authority_level">
+                            <div className="level_left">
+                                <div className="level_title">권한 레벨</div>
+                                <div className="level_name">{authorityLevel}</div>
+                            </div>
+                            <div className="level_right">{authorityLevelKr}</div>
+                        </div>
+                        <div className="authority_lists">
+                            <div className="list_item">
+                                <div className="icon">
+                                    <img src={Check} alt="Check" />
+                                </div>
+                                <div className="text">데이터 조회</div>
+                            </div>
+                            <div className="list_item">
+                                <div className="icon">
+                                    <img src={Check} alt="Check" />
+                                </div>
+                                <div className="text">중재 승인</div>
+                            </div>
+                            <div className="list_item">
+                                <div className="icon">
+                                    <img src={Check} alt="Check" />
+                                </div>
+                                <div className="text">공지 발송</div>
+                            </div>
+                            <div className="list_item">
+                                <div className="icon">
+                                    <img src={Check} alt="Check" />
+                                </div>
+                                <div className="text">시스템 설정</div>
+                            </div>
+                            <div className="list_item">
+                                <div className="icon">
+                                    <img src={Check} alt="Check" />
+                                </div>
+                                <div className="text">IoT 관리</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="infra">
+                <div className="infra_title">
+                    <div className="icon">
+                        <img src={Network} alt="Network" />
+                    </div>
+                    <div className="text">
+                        <div className="title">IoT 인프라 및 장비 관리</div>
+                        <div className="caption">아두이노 센서 통합 대시보드</div>
+                    </div>
+                </div>
+                <div className="infra_info">
+                    <div className="online">
+                        <div className="title">온라인 센서</div>
+                        <div className="value">{onlineSensors}개</div>
+                    </div>
+                    <div className="offline">
+                        <div className="title">오프라인 센서</div>
+                        <div className="value">{offlineSensors}개</div>
+                    </div>
+                    <div className="battery">
+                        <div className="title">평균 배터리</div>
+                        <div className="value">{averageBattery}%</div>
+                    </div>
+                    <div className="need_adjust">
+                        <div className="title">영점 조정 필요</div>
+                        <div className="value">{needAdjustment}개</div>
+                    </div>
+                </div>
+                <div className="adjust_list">
+                    <div className="title">영점 조절(Calibration) 리스트</div>
+                    <div className="item_list">
+                        <div className="item_left">
+                            <div className="icon">
+                                <img src={Wrench} alt="Wrench" />
+                            </div>
+                            <div className="text">
+                                <div className="sensor_location">A동 304호</div>  {/* 추후 API 연동 예정 */}
+                                <div className="sensor_info">센서 ID: SN-A304-01 · 마지막 점검: 2026.03.15</div>  {/* 추후 API 연동 예정 */}
+                            </div>
+                        </div>
+                        <div className="item_right">원격 영점 조절</div>
+                    </div>
+                    <div className="item_list">
+                        <div className="item_left">
+                            <div className="icon">
+                                <img src={Wrench} alt="Wrench" />
+                            </div>
+                            <div className="text">
+                                <div className="sensor_location">A동 705호</div>  {/* 추후 API 연동 예정 */}
+                                <div className="sensor_info">센서 ID: SN-A705-01 · 마지막 점검: 2026.02.28</div>  {/* 추후 API 연동 예정 */}
+                            </div>
+                        </div>
+                        <div className="item_right">원격 영점 조절</div>
+                    </div>
+                    <div className="item_list">
+                        <div className="item_left">
+                            <div className="icon">
+                                <img src={Wrench} alt="Wrench" />
+                            </div>
+                            <div className="text">
+                                <div className="sensor_location">C동 1505호</div>  {/* 추후 API 연동 예정 */}
+                                <div className="sensor_info">센서 ID: SN-C1505-01 · 마지막 점검: 2026.03.25</div>  {/* 추후 API 연동 예정 */}
+                            </div>
+                        </div>
+                        <div className="item_right">원격 영점 조절</div>
+                    </div>
+                </div>
+            </div>
+            <div className="individualize">
+                <div className="individualize_title">
+                    <div className="icon">
+                        <img src={Setting} alt="Setting" />
+                    </div>
+                    <div className="text">
+                        <div className="title">개인 맞춤형 업무 설정</div>
+                        <div className="caption">알림 임계값 및 근무 시간 관리</div>
+                    </div>
+                </div>
+                <div className="dB_setting">
+                    <div className="title">
+                        <div className="icon">
+                            <img src={Goal} alt="Goal" />
+                        </div>
+                        <div className="text">긴급 알림 임계값 설정</div>
+                    </div>
+                    <div className="adjust_value">
+                        <div className="dB_adjust">
+                            <div className="dB_title">소음 레벨 (dB)</div>
+                            <input type="text" name="dBThreshold" id="dBThreshold" />
+                        </div>
+                        <div className="time_adjust">
+                            <div className="time_title">지속 시간 (분)</div>
+                            <input type="text" name="timeThreshold" id="timeThreshold" />
+                        </div>
+                    </div>
+                    <div className="caption">현재 설정: 40dB 이상의 소음이 10분 이상 지속될 때 브라우저 알림 수신</div>
+                </div>
+                <div className="alarm_setting">
+                    <div className="title">
+                        <div className="title_left">
+                            <div className="icon">
+                                <img src={Alarm} alt="Alarm" />
+                            </div>
+                            <div className="text">
+                                <div className="alarm_title">업무 시간외 알림 차단</div>
+                                <div className="caption">퇴근 후 알림을 자동으로 차단합니다</div>
+                            </div>
+                        </div>
+                        <div className="title_right">
+                            <label className="switch">
+                                <input
+                                    type="checkbox"
+                                />
+                                <span className="slider"></span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="adjust_value">
+                        <div className="time_title">근무 시간 설정</div>
+                        <div className="time_adjust">
+                            <input type="text" name="startTimeThreshold" id="startTimeThreshold" />
+                            <div className="tilde">~</div>
+                            <input type="text" name="endTimeThreshold" id="endTimeThreshold" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="etc">
+                <div className="etc_title">
+                    <div className="icon">
+                        <img src={Key} alt="Key" />
+                    </div>
+                    <div className="text">
+                        <div className="title">외부 서비스 연계 관리</div>
+                        <div className="caption">API 연동 상태 및 인증 키 관리</div>
+                    </div>
+                </div>
+                <div className="list_item">
+                    <div className="item_left">
+                        <div className="icon">
+                            <img src={Check} alt="Check" />
+                        </div>
+                        <div className="text">
+                            <div className="name">층간소음 이웃사이센터</div>
+                            <div className="description">국가소음정보센터 API</div>
+                        </div>
+                    </div>
+                    <div className="item_right">정상</div>
+                </div>
+                <div className="list_item">
+                    <div className="item_left">
+                        <div className="icon">
+                            <img src={Check} alt="Check" />
+                        </div>
+                        <div className="text">
+                            <div className="name">공공기관 상담 센터</div>
+                            <div className="description">1661-2642 연계 시스템</div>
+                        </div>
+                    </div>
+                    <div className="item_right">정상</div>
+                </div>
+                <div className="list_item_adjust">
+                    <div className="item_left">
+                        <div className="icon">
+                            <img src={Log} alt="Log" />
+                        </div>
+                        <div className="text">
+                            <div className="name">클라우드 백업 서비스</div>
+                            <div className="description">AWS S3 연동</div>
+                        </div>
+                    </div>
+                    <div className="item_right">키 갱신</div>
+                </div>
+            </div>
         </div>
     )
 }
